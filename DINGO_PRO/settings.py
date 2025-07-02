ca25c1bbf409e25ef08a1ed572a572a767c9d5f5
asnafathima1204 <asnafathima1204@gmail.com>
2025-07-02 22:47:54 +0530

    csrf_token

--- DINGO_PRO/settings.py
+++ DINGO_PRO/settings.py
@@ -140,3 +140,6 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if not DEBUG:  # Production
+    CSRF_TRUSTED_ORIGINS = ['https://dingo-n29j.onrender.com']
