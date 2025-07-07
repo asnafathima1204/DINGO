ec21e3801d33951a69fceed23edd4e3a09d4782a
asnafathima1204 <asnafathima1204@gmail.com>
2025-07-07 16:54:17 +0530

    browser back

--- DINGO_PRO/settings.py
+++ DINGO_PRO/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'DINGO_APP.middleware.DisableBackButtonMiddleware',
 ]
 
 ROOT_URLCONF = 'DINGO_PRO.urls'
